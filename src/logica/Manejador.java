67645bd36d1705bf64c612df091f3d2444b3fcc1
Fralez <franco.gonzalez.burgos@gmail.com>
2019-09-30 11:03:19 -0300

    Manejadores creados y empezados

--- src/logica/Manejador.java
+++ src/logica/Manejador.java
@@ -1,5 +1,60 @@
-package logica;
-
-public class Manejador {
-
-}
+//package logica;
+//
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+//import java.util.ArrayList;
+//import java.util.Date;
+//
+//public class Manejador {
+//	private Manejador() {
+//	}
+//
+//	private static final Manejador instance = new Manejador();
+//
+//	public static Manejador getInstance() {
+//		return instance;
+//	}
+//
+//	public void altaUsuario(int ci, String nombre, String apellido, String mail, String password, TipoUsuario tipo,
+//			Orientacion orient) {
+//		// void
+//	}
+//
+//	public Usuario consultaUsuario(int id) {
+//		// return Usuario
+//	}
+//
+//	public Usuario buscarUsuario(int ci) {
+//		
+//	}
+//
+//	public void modificarDatosUsuario(String nombre, String apellido, String mail, String password) {
+//		// void
+//	}
+//
+//	public ArrayList<Usuario> listarUsuariosExistentes() {
+//		// return ArrayList<Usuario>
+//	}
+//
+//	public void altaPrestamo(int id, int codigoISBN, Date fechaPres, Date fechaDev, Boolean devuelto) {
+//		// void
+//	}
+//
+//	public Usuario consultarPrestamosUsuario(int idUsuario) {
+//		// return Usuario
+//	}
+//
+//	public ArrayList<Prestamo> listarPrestamos() {
+//		// return ArrayList<Prestamo>
+//	}
+//
+//	public void altaLibro(int codigoISBN) {
+//		// void
+//	}
+//
+//	public void darDeBajaUnPrestamo(int idUsuario, int idPrestamo) {
+//		// void
+//	}
+//
+//}
